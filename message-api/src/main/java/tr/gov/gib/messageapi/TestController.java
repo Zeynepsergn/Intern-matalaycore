9644fa858b7788b71f646cf597b12f9413f6f9ff
Melih Atalay <melihatalayy@gmail.com>
2023-03-03 01:08:57 +0300

    Logback.xml eklendi.

--- message-api/src/main/java/tr/gov/gib/messageapi/TestController.java
+++ message-api/src/main/java/tr/gov/gib/messageapi/TestController.java
@@ -1,6 +1,9 @@
 package tr.gov.gib.messageapi;
 
 import org.mapstruct.factory.Mappers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +18,12 @@ import tr.gov.gib.mapping.ServiceMapper;
 public class TestController {
 
     private final ServiceMapper serviceMapper = Mappers.getMapper(ServiceMapper.class);
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/test")
     @ResponseBody
     public ResponseEntity<ServiceReturnObject<String>> getHealth(){
+        LOG.info("Loglama yapildi.");
         ServiceReturnObject<String> serviceReturnObject = serviceMapper.getServiceReturnObject("Service is alive.",ServiceEnum.OK);
         return ResponseEntity.ok(serviceReturnObject);
     }
